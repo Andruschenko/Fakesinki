d5e807d24bb2fbab847d091c0539f91ec8a57862
0ki <git@kirils.org>
2016-11-26 14:27:29 +0200

    fixed logic error

--- src/prototype.userConfidence.py
+++ src/prototype.userConfidence.py
@@ -19,8 +19,17 @@ def userConfidence(NT,NS,TA,CS,TT,SE):
 	confidence=normalize(NT,4,1)+normalize(NS,5,0)+normalize(TA,0.3,0.05)+normalize(CS,3.0,0)+normalize(TT,15,1)
 	if(TA>0.5):confidence = confidence / 2
 	#confidence now is 0..5
-	return abs(SE - confidence/10)
+	return abs(SE - 0.5 + confidence/10)
 	
 	
 	
-print userConfidence(1, 0, 0.3, 100, 5.3, 1)
+test=userConfidence(1, 0, 0.3, 100, 5.3, 1)
+print "UC:",test
+print "User thinks that this is",
+if test<0.1: print "TOTALLY FALSE"
+elif test<0.3: print "FALSE"
+elif test<0.5: print "FALSE"
+elif test<0.7: print "HALF TRUE"
+elif test<0.9: print "MOSTLY TRUE"
+else: print "TRUE"
+
