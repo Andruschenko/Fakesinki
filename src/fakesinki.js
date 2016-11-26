db4058076988ec9918ee220cca3b4d50c14481ad
Andre Kovac <me@andrekovac.com>
2016-11-26 12:58:13 +0200

    Add react-native-swipe-cards example

---
+++ src/fakesinki.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import App from './App';
+
+export default function native() {
+  AppRegistry.registerComponent('Fakesinki', () => App);
+}
